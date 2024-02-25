cde6f3e0f84ae9340ab452a1d538fa6102301f46
🌃梦幻◎星空🌃 <Fantastic.Dream.Starry.Sky@outlook.com>
2024-02-26 05:36:54 +0800

    :construction:新增项目启动类

---
+++ src/main/java/icu/fdss/BigEventApplication.java
@@ -0,0 +1,14 @@
+package icu.fdss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 🌃梦幻◎星空🌃
+ */
+@SpringBootApplication
+public class BigEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigEventApplication.class, args);
+    }
+}
